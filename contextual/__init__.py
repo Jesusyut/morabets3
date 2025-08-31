6a8f6a5ba4550ede3bbdec62ed4f43d315b6fdaa
Jesus Mora <jackedwithjesus@gmail.com>
2025-08-31 16:06:24 -0700

    fix(contextual): add legacy alias get_contextual_hit_rate (+cached) for odds_api compat

--- contextual/__init__.py
+++ contextual/__init__.py
@@ -1,3 +1,15 @@
 from .mlb import get_mlb_contextual_hit_rate, get_mlb_contextual_hit_rate_cached
 
-__all__ = ["get_mlb_contextual_hit_rate", "get_mlb_contextual_hit_rate_cached"]
+# Legacy aliases for backward compatibility
+def get_contextual_hit_rate(*args, **kwargs):
+    return get_mlb_contextual_hit_rate(*args, **kwargs)
+
+def get_contextual_hit_rate_cached(*args, **kwargs):
+    return get_mlb_contextual_hit_rate_cached(*args, **kwargs)
+
+__all__ = [
+    "get_mlb_contextual_hit_rate",
+    "get_mlb_contextual_hit_rate_cached",
+    "get_contextual_hit_rate",
+    "get_contextual_hit_rate_cached",
+]
